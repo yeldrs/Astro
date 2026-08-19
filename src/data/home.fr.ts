2bf40d04339b246e140dbc6e5996bf580a63c03a
yeldrs <yass.elidrissi97@gmail.com>
2026-08-19 11:01:54 +0200

    Copy Update home.fr.ts intro
    MAJ de l'intro en francais sur la page index

--- src/data/home.fr.ts
+++ src/data/home.fr.ts
@@ -5,6 +5,7 @@ import type { HomeData } from "./home.types";
 export const home: HomeData = {
   heroTitle: "Salut, moi c'est Yassine",
 heroSubtitle:
-  "Je suis product designer, je transforme les insights utilisateurs en expériences mesurables, à forte valeur et surtout...",
+  "Je suis product designer, je transforme les insights utilisateurs en expériences authentiques, mesurables, et surtout...",
 heroSubtitleBreak: "... conçues pour les gens.",
+
 };
